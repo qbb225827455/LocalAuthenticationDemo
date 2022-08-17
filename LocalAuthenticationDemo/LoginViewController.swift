20f4e2b62856d51c9f2df15a64a0fbbd858e4ae8
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-08-18 00:13:23 +0800

    git init

---
+++ LocalAuthenticationDemo/LoginViewController.swift
@@ -0,0 +1,61 @@
+//
+//  LoginViewController.swift
+//  LocalAuthenticationDemo
+//
+//  Created by 陳鈺翔 on 2022/8/18.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    var blurEffectView: UIVisualEffectView!
+    
+    @IBOutlet weak var backgroundImageView:UIImageView!
+    @IBOutlet weak var loginView:UIView! {
+        didSet {
+            loginView.layer.cornerRadius = 30
+        }
+    }
+    
+    @IBOutlet weak var emailTextField:UITextField! {
+        didSet {
+            emailTextField.layer.cornerRadius = 10
+        }
+    }
+    
+    @IBOutlet weak var passwordTextField:UITextField! {
+        didSet {
+            passwordTextField.layer.cornerRadius = 10
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        backgroundImageView.image = UIImage(named: "background")
+        let blurEffect = UIBlurEffect(style: .dark)
+        blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        
+        showLoginDialog()
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        blurEffectView.frame = view.bounds
+    }
+    
+    func showLoginDialog() {
+        
+        loginView.isHidden = false
+        loginView.transform = CGAffineTransform(translationX: 0, y: -700)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            
+            self.loginView.transform = CGAffineTransform.identity
+            
+        }, completion: nil)
+        
+    }
+}
---
+++ LocalAuthenticationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalAuthenticationDemo
+//
+//  Created by 陳鈺翔 on 2022/8/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
